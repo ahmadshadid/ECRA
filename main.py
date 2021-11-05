e0cd3c5ea43e6e36314e650c50f441b444e30b74
Ramzi BEN AMARA <ramzi1.benamara@gmail.com>
2021-11-05 19:38:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from binance.client import Client
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import timedelta
-
-api_key = ""
-api_secret = ""
+from plotly.subplots import make_subplots
+api_key = "uGiXeptXdpw1MaptLsVJnXEcCIxLz7q766XUF4lkDnOQn1WzzjzbnGsvmyBBR4r4"
+api_secret = "nz5Sw5IKOPQVPl3hYJTywuJvaoQbQKJ9OMXFi6rV1wWs4h0EpHuscVymH2JhH2Il"
 client = Client(api_key, api_secret)
 
 st.title('Cryptos Exploration')
@@ -131,10 +131,69 @@ fig4.update_xaxes(
 fig4.update_yaxes(showgrid=False,title_text="<b>close "+ str(selected_crypto)+"</b>  ", secondary_y=False)
 fig4.update_yaxes(showgrid=False,title_text="<b>close_btc</b> ", secondary_y=True)
 
-# Bollinger bands
 st.header('**Closing Price**')
 
 st.plotly_chart(fig4)
+##ats exploration
+df_initial_ats = pd.read_csv('index_ATS.csv',sep=',')
+df_initial_ats['timeStamp'] = pd.to_datetime(df_initial_ats['date'])
+df_initial_ats.replace([np.inf, -np.inf], np.nan, inplace=True)
+df_initial_ats.drop(['Unnamed: 0', 'date'], axis=1,inplace=True)
+df_initial_ats.set_index('timeStamp',inplace=True)
+df_initial_ats["mean"] = df_initial_ats.mean(axis=1)
+
+##tps exploration
+df_initial_tps = pd.read_csv('index_TPS.csv',sep=',')
+df_initial_tps['timeStamp'] = pd.to_datetime(df_initial_tps['date'])
+df_initial_tps.replace([np.inf, -np.inf], np.nan, inplace=True)
+df_initial_tps.drop(['Unnamed: 0', 'date'], axis=1,inplace=True)
+df_initial_tps.set_index('timeStamp',inplace=True)
+df_initial_tps["mean"] = df_initial_tps.mean(axis=1)
+
+#substring a string to have btc not btcusdt
+indexfi=selected_crypto.find('USDT')
+selected_crypto_fi=selected_crypto[0:indexfi]
+
+
+fig5 = go.Figure()
+fig5 = make_subplots(rows=3, cols=1)
+
+fig5.add_trace(go.Scatter(x=df_initial_ats.index, y=df_initial_ats[selected_crypto_fi],name="ats "+selected_crypto_fi),row=1, col=1)
+fig5.add_trace(go.Scatter(x=df_initial_ats.index, y=df_initial_ats[selected_crypto_fi].rolling(6).mean(),name="MAVG_ats "+selected_crypto_fi,visible='legendonly'),row=1, col=1)
+
+fig5.add_trace(go.Scatter(x=df_initial_ats.index, y=df_initial_ats["mean"],name='ats_weighted_global'),row=2, col=1)
+fig5.add_trace(go.Scatter(x=df_initial_ats.index, y=df_initial_ats["mean"].rolling(6).mean(),name="MAVG_ats_weighted_global ",visible='legendonly'),row=2, col=1,secondary_y=False)
+fig5.add_trace(go.Scatter(x=rawfinal.timestamp, y=rawfinal['close'],name='close' + ' ' + str(selected_crypto)),row=3, col=1)
+
+
+fig5.update_layout(title_text="Side By Side ATS "+selected_crypto+" ATS GLOBAL" ,paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',width=800,
+    height=500)
+
+
+st.header('**Side By Side ATS**')
+
+st.plotly_chart(fig5)
+
+
+
+fig6 = go.Figure()
+fig6 = make_subplots(rows=3, cols=1)
+
+fig6.add_trace(go.Scatter(x=df_initial_tps.index, y=df_initial_tps[selected_crypto_fi],name="tps "+selected_crypto_fi),row=1, col=1)
+fig6.add_trace(go.Scatter(x=df_initial_tps.index, y=df_initial_tps[selected_crypto_fi].rolling(6).mean(),name="MAVG_tps "+selected_crypto_fi,visible='legendonly'),row=1, col=1)
+
+fig6.add_trace(go.Scatter(x=df_initial_tps.index, y=df_initial_tps["mean"],name='tps_weighted_global'),row=2, col=1)
+fig6.add_trace(go.Scatter(x=df_initial_tps.index, y=df_initial_tps["mean"].rolling(6).mean(),name="MAVG_tps_weighted_global ",visible='legendonly'),row=2, col=1)
+fig6.add_trace(go.Scatter(x=rawfinal.timestamp, y=rawfinal['close'],name='close' + ' ' + str(selected_crypto)),row=3, col=1)
+
+
+fig6.update_layout(title_text="Side By Side TPS "+selected_crypto+" TPS GLOBAL" ,paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',width=800,
+    height=500)
+
+
+st.header('**Side By Side TPS**')
+
+st.plotly_chart(fig6)
 
 
 
