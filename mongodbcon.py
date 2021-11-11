f242f1e0de1a79e57f9131eebace7423555195ff
Ramzi BEN AMARA <ramzi1.benamara@gmail.com>
2021-11-11 12:13:36 +0100

    Update mongodbcon.py

--- mongodbcon.py
+++ mongodbcon.py
@@ -3,18 +3,18 @@ import pymongo
 from pymongo import MongoClient
 import pandas as pd
 
+# import ssl
 
-#import ssl
-
-#market_data = pymongo.MongoClient(
+# market_data = pymongo.MongoClient(
 #    "mongodb+srv://doadmin:2YjQ6con413i87R0@market-watch-74d794d0.mongo.ondigitalocean.com/admin?authSource=admin&tls=true&tls=true",
 #    ssl_cert_reqs=ssl.CERT_NONE)
 ##prod_environnment
-#market_data = pymongo.MongoClient( "mongodb+srv://doadmin:2YjQ6con413i87R0@market-watch-74d794d0.mongo.ondigitalocean.com/admin?authSource=admin&tls=true&tls=true")
+# market_data = pymongo.MongoClient( "mongodb+srv://doadmin:2YjQ6con413i87R0@market-watch-74d794d0.mongo.ondigitalocean.com/admin?authSource=admin&tls=true&tls=true")
 
 
 # get_dev database
-market_data_dev = pymongo.MongoClient("mongodb+srv://ramziadmin:ramziadmin@cluster2.e6ban.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+market_data_dev = pymongo.MongoClient(
+    "mongodb+srv://ramziadmin:ramziadmin@cluster2.e6ban.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 klines = market_data_dev.get_database('klines_dev')
 
@@ -23,11 +23,11 @@ custom_index = klines['custom_index']
 
 
 @st.cache
-def load_data(crypto):
+def load_data(crypto="BTCUSDT"):
     cursor = crypto_klines.find({"symbol": crypto}, {"_id": 0, "data": 1})
     b = pd.DataFrame(cursor)
     raw = pd.DataFrame(b['data'][0])
-    #raw[0] = pd.to_datetime(raw[0], unit='ms')
+    # raw[0] = pd.to_datetime(raw[0], unit='ms')
     raw.columns = ['timestamp', 'close', 'tps', 'rps', 'tps_std_110', 'rps_std_110', 'tps_std_1700', 'rps_std_1700']
 
     # print(raw)
